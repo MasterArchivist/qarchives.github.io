6214e6f44a596c396b7627d0bd2729f6f9464a95
Master Archivist <masterarchivist@protonmail.com>
2019-11-17 19:36:06 +0530

    css updated

--- head.php
+++ head.php
@@ -5,4 +5,4 @@
 <meta name="author" content="MasterArchivist" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="stylesheet" href="styles/css_20191114_1308_et.css" />
+<link rel="stylesheet" href="styles/css_20191117_1111_et.css" />
