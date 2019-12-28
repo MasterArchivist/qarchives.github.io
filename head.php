19a6d801437c419d418f4b65e0250995785f4b93
Master Archivist <masterarchivist@protonmail.com>
2019-12-28 01:57:13 -0500

    trying to solve merging issues

--- head.php
+++ head.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <meta charset="UTF-8" />
 <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <![endif]-->
 <meta name="description" content="List of, and direct links to, all QResearch General and Non-QResearch qanon posts and threads on 4chan, 8chan, 8kun and anywhere else on the internet." />
@@ -5,4 +6,13 @@
 <meta name="author" content="MasterArchivist" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+=======
+<meta charset="UTF-8" />
+<!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <![endif]-->
+<meta name="description" content="List of, and direct links to, all QResearch General and Non-QResearch qanon posts and threads on 4chan, 8chan, 8kun and anywhere else on the internet." />
+<meta name="keywords" content="Q, q, qanon, Q-anon, Qanon, masterarchivist, master, archivist, ma, CBTS, cbts, Calm, Before, The, Storm, calm, before, the, storm, great, awakening, greatawakening, research, qresearch" />
+<meta name="author" content="MasterArchivist" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+>>>>>>> 7bc146841a1b7e2c0ad8b5fe7b95b4f6369d4600
 <link rel="stylesheet" href="styles/css_20191218_0706_et.css" />
