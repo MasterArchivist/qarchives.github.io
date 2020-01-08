62ad351a6f5459f2c67309d661f3ba371525dc11
Master Archivist <masterarchivist@protonmail.com>
2020-01-08 14:23:44 -0500

    cleaned up

--- 4pol_q_posts.php
+++ 4pol_q_posts.php
@@ -97,12 +97,12 @@
 <tr><td id="sno"><br /><br /> 83.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148020278" target="_blank"><img src="images/4pol/148020278.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br /> 84.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148021501" target="_blank"><img src="images/4pol/148021501.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br /> 85.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148027165" target="_blank"><img src="images/4pol/148027165.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 86.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148022342" target="_blank"><img src="images/4pol/148022342.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 87.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148023976" target="_blank"><img src="images/4pol/148023976.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 88.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148025825" target="_blank"><img src="images/4pol/148025825.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 89.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148022145" target="_blank"><img src="images/4pol/148022145.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 90.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148029633" target="_blank"><img src="images/4pol/148029633.PNG" /></a></td></tr>
-<tr><td id="sno"><br /><br /> 91.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148029962" target="_blank"><img src="images/4pol/148029962.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 86.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148022145" target="_blank"><img src="images/4pol/148022145.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 87.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148022342" target="_blank"><img src="images/4pol/148022342.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 88.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148023976" target="_blank"><img src="images/4pol/148023976.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 89.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148029633" target="_blank"><img src="images/4pol/148029633.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 90.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148029962" target="_blank"><img src="images/4pol/148029962.PNG" /></a></td></tr>
+<tr><td id="sno"><br /><br /> 91.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/148016618/#148025825" target="_blank"><img src="images/4pol/148025825.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br /> 92.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148031295" target="_blank"><img src="images/4pol/148031295.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br /> 93.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148032210" target="_blank"><img src="images/4pol/148032210.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br /> 94.</td><td>Gen.: 54  <hr /><a href="https://archive.4plebs.org/pol/thread/148019103/#148032910" target="_blank"><img src="images/4pol/148032910.PNG" /></a></td></tr>
@@ -245,7 +245,7 @@
 <tr><td id="sno"><br /><br />231.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/150864944/#150876856" target="_blank"><img src="images/4pol/150876856.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br />232.</td><td>Gen.: 611 <hr /><a href="https://archive.4plebs.org/pol/thread/151549034/#151561953" target="_blank"><img src="images/4pol/151561953.PNG" /></a></td></tr>
 <tr><td id="sno"><br /><br />233.</td><td>non-cbts  <hr /><a href="https://archive.4plebs.org/pol/thread/153317403/#153323368" target="_blank"><img src="images/4pol/153323368.PNG" /></a></td></tr>
-<tr><td></td><td></td><td></td><td></td><td></td></tr>
+<tr><td></td><td></td></tr>
 </table>
 <!-- ---------------------------------------------------------------------- TABLE 3 Ends ---------------------------------------------------------------------- -->
 
