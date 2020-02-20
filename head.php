2a799aef063ab58f019ce939b056d67ad26a8cf9
Master Archivist <masterarchivist@protonmail.com>
2020-02-20 13:59:33 -0500

    del

--- head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8" />
-<!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <![endif]-->
-<meta name="description" content="List of, and direct links to, all QResearch General and Non-QResearch qanon posts and threads on 4chan, 8chan, 8kun and anywhere else on the internet." />
-<meta name="keywords" content="Q, q, qanon, Q-anon, Qanon, masterarchivist, master, archivist, ma, CBTS, cbts, Calm, Before, The, Storm, calm, before, the, storm, great, awakening, greatawakening, research, qresearch" />
-<meta name="author" content="MasterArchivist" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta http-equiv = "refresh" content = "0; url = q_posts.html#lqp" />
-
-<link rel="stylesheet" href="styles/css_20200121_0600_et.css" />
